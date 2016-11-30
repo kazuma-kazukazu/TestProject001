2279369fee72361be1222bbbbdab9c9c2065af56
tetsu_ts <tetsu_ts@SurfaceT>
2016-11-30 22:39:41 +0900

    コンフリクト覚悟で関数一つ追加 testbrunch011

--- src/main/hello.java
+++ src/main/hello.java
@@ -4,8 +4,14 @@
 public class HelloWorld1 {
     public static void main(String[] args){
         System.out.println("Hello, world.contents1");
+        this.subfunc111(10);
         this.subfuncAAA(20);
 
+    }
+
+    // 呼び出し関数その２
+    public int subfunc111(int no){
+        System.out.println("call subfunction 111");
     }
 
     // 呼び出し関数その２
