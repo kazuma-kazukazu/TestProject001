5599f56870f5c4221639700da2c8b9c200ed53bd
tetsu_ts <tetsu_ts@SurfaceT>
2016-11-30 22:10:40 +0900

    初回修正 from testbrunch1

--- src/main/hello.java
+++ src/main/hello.java
@@ -4,5 +4,13 @@
 public class HelloWorld1 {
     public static void main(String[] args){
         System.out.println("Hello, world.contents1");
+        this.subfuncAAA(20);
+
     }
+
+    // 呼び出し関数その２
+    public int subfuncAAA(int no){
+        System.out.println("call subfunction1");
+    }
+
 }
